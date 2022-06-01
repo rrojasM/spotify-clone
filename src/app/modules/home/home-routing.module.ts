5cddcc5bfa40e229f4f19e3fff1288c777f23058
Rufino Rojas <rojasru@globalhitss.com>
2022-06-01 12:41:28 -0500

    se agregaron componentes pages

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomePagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
